fd7e90602806d1d92a1564ba39ad68b125e43783
haprodeptrai123 <haprodeptrai123@gmail.com>
2021-07-21 11:16:58 +0700

    Create frmBirthday.designer.cs

---
+++ frmBirthday.designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProjManagement
+{
+    partial class frmBirthday
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LivBirthDay = new System.Windows.Forms.ListView();
+            this.empSSN = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.epmName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.depName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.empBirthDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(437, 436);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.LivBirthDay);
+            this.groupBox1.Location = new System.Drawing.Point(101, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(749, 294);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "            The list of employees with date of birth in month";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjManagement.Properties.Resources.apple;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 15);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LivBirthDay
+            // 
+            this.LivBirthDay.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.empSSN,
+            this.epmName,
+            this.depName,
+            this.empBirthDate});
+            this.LivBirthDay.Location = new System.Drawing.Point(56, 41);
+            this.LivBirthDay.Name = "LivBirthDay";
+            this.LivBirthDay.Size = new System.Drawing.Size(637, 235);
+            this.LivBirthDay.TabIndex = 3;
+            this.LivBirthDay.UseCompatibleStateImageBehavior = false;
+            this.LivBirthDay.View = System.Windows.Forms.View.Details;
+            // 
+            // empSSN
+            // 
+            this.empSSN.Text = "EmpSSN";
+            this.empSSN.Width = 120;
+            // 
+            // epmName
+            // 
+            this.epmName.Text = "EmpName";
+            this.epmName.Width = 150;
+            // 
+            // depName
+            // 
+            this.depName.Text = "DepName";
+            this.depName.Width = 200;
+            // 
+            // empBirthDate
+            // 
+            this.empBirthDate.Text = "EmpBirthDate";
+            this.empBirthDate.Width = 180;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProjManagement.Properties.Resources.fptlogo;
+            this.pictureBox2.Location = new System.Drawing.Point(829, 436);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(78, 35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mouser", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(280, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 35);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Your Birthdays in month";
+            // 
+            // frmBirthday
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmBirthday";
+            this.Text = "Birthday";
+            this.Load += new System.EventHandler(this.Birthday_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView LivBirthDay;
+        private System.Windows.Forms.ColumnHeader empSSN;
+        private System.Windows.Forms.ColumnHeader epmName;
+        private System.Windows.Forms.ColumnHeader empBirthDate;
+        private System.Windows.Forms.ColumnHeader depName;
+        private System.Windows.Forms.Label label1;
+    }
+}
